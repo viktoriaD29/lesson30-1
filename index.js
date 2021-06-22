137372de8231179325fbd2be9af71f9c43498269
Viktoria <doduchenko29@gmail.com>
2021-06-22 14:41:40 +0300

    add

---
+++ index.js
@@ -0,0 +1,35 @@
+const addImage = (imgSrc) => {
+  const p = new Promise((resolveCb, rejectCb) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+    const container = document.querySelector('.page');
+    container.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolveCb({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () =>
+      rejectCb(new Error('Image load is failed...'))
+    );
+  });
+  return p;
+};
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const resultPromise = addImage(imgSrc);
+resultPromise.then((data) => console.log(data));
+/*const onImageLoaded = (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};*/
